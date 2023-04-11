b4d87798b7f421262ffc4aad6dabefca5f72b90a
Aayush Rijal <arijal@aiims.com.au>
2023-04-12 09:35:38 +1000

    Design 3 done

---
+++ app/thankyou.php
@@ -0,0 +1,33 @@
+<?php include __DIR__ . '/functions.php'; ?>
+
+<?php include __DIR__ . '/src/header.php'; ?>
+
+<!-- Design 1 -->
+
+<!-- Event snippet for Quote Form (Business Finance 1)** conversion page -->
+
+<script>
+    gtag('event', 'conversion', {
+        'send_to': 'AW-714867981/WxaICJ3hpo8YEI2K8NQC'
+    });
+</script>
+
+
+
+<!-- Design 2 -->
+
+<!-- Event snippet for Quote Form (Business Finance 2)** conversion page -->
+
+<script>
+    gtag('event', 'conversion', {
+        'send_to': 'AW-714867981/Q6q2CIuhqpcYEI2K8NQC'
+    });
+</script>
+
+<div class="thankyou-wrapper text-white text-center py-3" style="background-color: green;">
+    Thank you! Your message has been sent. We will contact you shortly.
+</div>
+
+<?php include __DIR__ . '/src/main.php'; ?>
+
+<?php include __DIR__ . '/src/footer.php'; ?>
